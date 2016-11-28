1b97b3028a4b3db86165027d7d8b42489cdebefa
rjisoo <jisoo.cc@gmail.com>
2016-11-28 12:34:33 -0500

    setup game room

--- game/lobby/GameRoom.js
+++ game/lobby/GameRoom.js
@@ -39,8 +39,6 @@ LunarAdventure.Gameroom.prototype = {
 		this.leaveGameButton.inputEnabled = true;
 		this.leaveGameButton.events.onInputDown.add(this.leaveGameAction, {gameId: this.gameId, LunarAdventure: this})
 
-		// this.leaveGameButton.setDownSound(buttonClickSound);
-		
 		this.characterSquares = this.drawCharacterSquares(6);
 		this.characterImages = [];
 		this.numPlayersInGame = 0;
@@ -54,29 +52,22 @@ LunarAdventure.Gameroom.prototype = {
 		socket.on("start game on client", this.startGame);
 	},
 
-	update: function() {
-	},
-
 	drawCharacterSquares: function(numOpenings) {
 		let characterSquares = [];
 		let xOffset, yOffset
 
 		for(let i = 0; i < numOpenings; i++) {
-			// let frame = i < numOpenings ? "lobby/slots/character_square_01.png" : "lobby/slots/character_square_02.png";
 			if (i <= 2) {
 				xOffset = width/5 * (i+1);
-				yOffset = height/10 * 2
+				yOffset = height/10 * 2;
 			} else {
-				xOffset = width/5 * (i-2)
-				yOffset = height/10 * 5
+				xOffset = width/5 * (i-2);
+				yOffset = height/10 * 5;
 			}
 			let frame = this.game.add.graphics(xOffset, yOffset);
 			frame.beginFill(0x222222, 0.8);
 			frame.drawRoundedRect(0,0, width/7, width/7, 10)
 			characterSquares[i] = frame
-			// characterSquares[i] = this.game.add.sprite(xOffset, yOffset, 'astronaut', frame);
-			// characterSquares[i].scale.setTo(0.5);
-			// characterSquares[i].anchor.set(-0.25)
 		}
 
 		return characterSquares;
@@ -85,56 +76,38 @@ LunarAdventure.Gameroom.prototype = {
 	populateCharacterSquares: function(data) {
 		this.numPlayersInGame = 0;
 		for(let playerId in data.players) {
-			// let color = data.players[playerId].color;
 			let userCharacter = this.game.add.image(this.characterSquares[this.numPlayersInGame].position.x, this.characterSquares[this.numPlayersInGame].position.y, 'astronaut');
 			userCharacter.scale.setTo(0.7);
 			this.characterImages[playerId] = userCharacter
 			this.numPlayersInGame++;
 		}
 
-		if(this.numPlayersInGame > 1) {
-			this.activateStartGameButton();
-		} else {
-			// this.minPlayerMessage.visible = true;
-		}
+		if(this.numPlayersInGame > 1) { this.activateStartGameButton(); } 
+		else { this.minPlayerMessage.visible = true; }
 	},
 
 	playerJoined: function(data) {
 		this.numPlayersInGame++;
-
 		let index = this.numPlayersInGame - 1;
-
 		let userCharacter = this.game.add.image(this.characterSquares[index].position.x, this.characterSquares[index].position.y, 'astronaut');
 			userCharacter.scale.setTo(0.7);
 			this.characterImages[data.id] = userCharacter
 
-		// this.characterImages[data.id] = this.game.add.image(this.characterSquares[index].position.x + characterOffsetX,
-		//  this.characterSquares[index].position.y + characterOffsetY, TEXTURES, "lobby/bomberman_head/bomberman_head_" +  data.color + ".png");
-
-		// Activate start game button if this is the second player to join the this.game.
-		if(this.numPlayersInGame == 2) {
-			this.activateStartGameButton();
-		}
+		if(this.numPlayersInGame == 2) { this.activateStartGameButton(); }
 	},
 
 	activateStartGameButton: function() {
-		// this.minPlayerMessage.visible = false;
-		// this.startGameButton.setFrames("lobby/buttons/start_game_button_02.png", "lobby/buttons/start_game_button_01.png");
-		// this.startGameButton.onInputUp.removeAll();
+		this.minPlayerMessage.visible = false;
 
 		this.startGameButton.clear();
 		this.startGameButton.beginFill(0xaa2222, 1);
 		this.startGameButton.drawRoundedRect(0,0, width/5, height/13, 10)
 		this.startGameButton.inputEnabled = true;
 		this.startGameButton.events.onInputDown.add(this.startGameAction, this);
-		// this.startGameButton.setDownSound(buttonClickSound);
 	},
 
 	deactivateStartGameButton: function() {
-		// this.minPlayerMessage.visible = true;
-		// this.startGameButton.setFrames("lobby/buttons/start_game_button_03.png", "lobby/buttons/start_game_button_03.png");
-		// this.startGameButton.onInputUp.removeAll();
-		// this.startGameButton.setDownSound(null);
+		this.minPlayerMessage.visible = true;
 		this.startGameButton.clear();
 		this.startGameButton.beginFill(0x888888, 1);
 		this.startGameButton.drawRoundedRect(0,0, width/5, height/13, 10)
@@ -143,10 +116,7 @@ LunarAdventure.Gameroom.prototype = {
 
 	playerLeft: function(data) {
 		this.numPlayersInGame--;
-
-		if(this.numPlayersInGame == 1) {
-			this.deactivateStartGameButton();
-		}
+		if(this.numPlayersInGame == 1) { this.deactivateStartGameButton(); }
 
 		for(let playerId in this.characterImages) {
 			this.characterImages[playerId].destroy();
@@ -154,13 +124,10 @@ LunarAdventure.Gameroom.prototype = {
 		this.populateCharacterSquares(data);
 	},
 
-	// When the "start" button is clicked, send a message to the server to initialize the this.game.
-	startGameAction: function() {
-		socket.emit("start game on server");
-	},
+	startGameAction: function() { socket.emit("start game on server"); },
 
 	leaveGameAction: function() {
-		socket.emit("leave pending game");
+		socket.emit("leave game room");
 		socket.removeAllListeners();
 		LunarAdventure.game.state.start("Lobby", true, false);
 	},
