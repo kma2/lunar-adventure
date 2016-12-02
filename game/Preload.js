d4d02c02d7ee3acdbd76b696120dc678fb83e114
Katy <kma2@wellesley.edu>
2016-12-02 15:31:32 -0500

    changed key assets

--- game/Preload.js
+++ game/Preload.js
@@ -6,54 +6,50 @@ LunarAdventure.Preload = function(){};
 LunarAdventure.Preload.prototype = {
 	preload: function() {
 
-	// loading progress bar
-	// this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 128, 'preloadbar');
-	// this.preloadBar.anchor.setTo(0.5);
-	// this.load.setPreloadSprite(this.preloadBar);
+		// loading progress bar
+		// this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 128, 'preloadbar');
+		// this.preloadBar.anchor.setTo(0.5);
+		// this.load.setPreloadSprite(this.preloadBar);
 
-	//load game assets
-	this.load.image('astronaut', 'images/TheAstronautWhite.png');
-	this.load.image('logo', 'images/logo.png');
-	// this.load.image('ship', 'images/rocket.png');
-	this.load.image('ship', 'images/new_rocket.png');
-	this.load.image('explosion', 'images/explosion.png');
-	this.load.physics('tracedTerrain', 'terrainPolygon.json');
-	this.load.image('terrain', 'images/terrain2.png');
-	this.load.image('starfield', 'images/starfield.png');
-	this.load.image('landingPad', 'images/landingPad.png');
-	this.load.image('crash', 'images/crash_message.png');
-	this.load.image('success', 'images/success_message.png');
-	this.load.image('submitBtn', 'images/submitButton.png');
+		//load game assets
+		this.load.image('astronaut', 'images/TheAstronautWhite.png');
+		this.load.image('logo', 'images/logo.png');
+		// this.load.image('ship', 'images/rocket.png');
+		this.load.image('ship', 'images/new_rocket.png');
+		this.load.image('explosion', 'images/explosion.png');
+		this.load.physics('tracedTerrain', 'terrainPolygon.json');
+		this.load.image('terrain', 'images/terrain2.png');
+		this.load.image('starfield', 'images/starfield.png');
+		this.load.image('landingPad', 'images/landingPad.png');
+		this.load.image('crash', 'images/crash_message.png');
+		this.load.image('success', 'images/success_message.png');
+		this.load.image('submitBtn', 'images/submitButton.png');
 
-	// obstacles
-	this.load.image('tinyObstacle', 'images/tinyObstacle.png');
-	this.load.image('smallObstacle', 'images/smallObstacle.png');
-	this.load.image('mediumObstacle', 'images/mediumObstacle.png');
-	this.load.image('largeObstacle', 'images/largeObstacle.png');
-	this.load.image('penalty10', 'images/penalty10.png');
-	this.load.image('penalty5', 'images/penalty5.png')
-	this.load.image('boundary', 'images/boundary.png');
+		// obstacles
+		this.load.image('tinyObstacle', 'images/tinyObstacle.png');
+		this.load.image('smallObstacle', 'images/smallObstacle.png');
+		this.load.image('mediumObstacle', 'images/mediumObstacle.png');
+		this.load.image('largeObstacle', 'images/largeObstacle.png');
+		this.load.image('penalty10', 'images/penalty10.png');
+		this.load.image('penalty5', 'images/penalty5.png')
+		this.load.image('boundary', 'images/boundary.png');
 
-	// UI
-	this.load.image('leftKeyUp', 'images/keyUI/leftUnpressed.png');
-	this.load.image('leftKeyDown', 'images/keyUI/leftPressed.png');
-	this.load.image('rightKeyUp', 'images/keyUI/rightUnpressed.png');
-	this.load.image('rightKeyDown', 'images/keyUI/rightPressed.png');
-	this.load.image('upKeyUp', 'images/keyUI/upUnpressed.png');
-	this.load.image('upKeyDown', 'images/keyUI/upPressed.png');
+		// UI
+		this.load.image('leftKeyUp', 'images/keyUI/leftUnpressed.png');
+		this.load.image('leftKeyDown', 'images/keyUI/leftPressed.png');
+		this.load.image('rightKeyUp', 'images/keyUI/rightUnpressed.png');
+		this.load.image('rightKeyDown', 'images/keyUI/rightPressed.png');
+		this.load.image('upKeyUp', 'images/keyUI/upUnpressed.png');
+		this.load.image('upKeyDown', 'images/keyUI/upPressed.png');
 
-	// A-Z
-	this.loadKeyImage.call(this, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''))
+		// A-Z
+		this.loadKeyImage.call(this, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''))
 
-	// UI key labels
-	this.load.image('landingArrow', 'images/landingArrow.png');
-	this.load.image('thrust', 'images/thrustUI.png');
-	this.load.image('rotateL', 'images/rotateLeftUI.png');
-	this.load.image('rotateR', 'images/rotateRightUI.png');
-
-	// multiplayer
-	this.load.image('W_upKeyUp', 'images/W_upKeyUp.png');
-	this.load.image('W_upKeyDown', 'images/W_upKeyDown.png');
+		// UI key labels
+		this.load.image('landingArrow', 'images/landingArrow.png');
+		this.load.image('thrust', 'images/thrustUI.png');
+		this.load.image('rotateL', 'images/rotateLeftUI.png');
+		this.load.image('rotateR', 'images/rotateRightUI.png');
 	},
 	loadKeyImage: function(arr) {
 		arr.forEach(char => {
