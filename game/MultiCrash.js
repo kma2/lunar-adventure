569c46a39bee64eaefab2c865343e71399d78c77
Katy <kma2@wellesley.edu>
2016-12-03 15:49:14 -0500

    commented crash states

--- game/MultiCrash.js
+++ game/MultiCrash.js
@@ -2,35 +2,40 @@ LunarAdventure.MultiCrash = function(){};
 
 LunarAdventure.MultiCrash.prototype = {
 	create: function() {
-
+		// array of high scores
 		let highScores;
 
+		// fetch high scores (cooperative mode) from database
 		fetch('/highScore/Cooperative')
 		.then(res => res.json())
 		.then(scoreList => {
 			highScores = scoreList;
+
+			// crash message
 			message = this.add.sprite(gameWidth/2 - 210, gameHeight/8, 'crash');
 			message.scale.setTo(0.6, 0.6);
 
 			this.game.add.text(gameWidth/3 - 20, gameHeight/3 - 60, `Better luck next time! Your journey was ${endGameTime} seconds.`, fontStyle);
 
-			//leaderBoard
+			// display leaderBoard
 			let yVal = gameHeight/3 + 30;
 			for (var i = 0; i < highScores.length; i++) {
 				this.game.debug.text(`${highScores[i].time}s   -   ${highScores[i].name}`, gameWidth/2 - 85, yVal);
 				yVal += 30
 			}
+
 			this.game.add.text(gameWidth/2.3 - 45, gameHeight - 150, "Press spacebar to play again", fontStyle);
 		})
-		.catch(err => console.error('error retrieving scores', err))
+		.catch(err => console.error('error retrieving scores', err));
 
+		// add physics and background image
 		this.physics.startSystem(Phaser.Physics.P2JS);
 		this.background = this.game.add.tileSprite(0, 0, gameWidth, gameHeight, 'starfield');
 
-		// creating static terrain
+		// create terrain
 		terrain = this.add.sprite(centerX, centerY, 'terrain');
-		terrain.anchor.set(0.5)
-		this.physics.p2.enable(terrain, false)
+		terrain.anchor.set(0.5);
+		this.physics.p2.enable(terrain, false);
 		terrain.body.static = true;
 		terrain.body.clearShapes();
 		terrain.body.loadPolygon('tracedTerrain', 'terrain');
@@ -39,9 +44,9 @@ LunarAdventure.MultiCrash.prototype = {
 	update: function() {
 		terrain.body.rotation -= 0.003;
 
-		//press spacebar to play again
+		// press spacebar to play again
 		if (cursors.spacebar.isDown) {
-			this.game.state.start('MainMenu')
+			this.game.state.start('MainMenu');
 		}
 	}
 };
