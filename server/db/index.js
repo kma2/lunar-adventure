407040d09c0d122074237bbfed5a8666b673de99
Brandon Culp <bculp513@gmail.com>
2016-12-18 13:56:59 -0500

    replaced heroku db link with new link that heroku generated

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const herokuDB = 'postgres://dmfyfyacyfpvid:eqr77jZOFtzXY3mX_nBpb2mJTH@ec2-54-75-244-145.eu-west-1.compute.amazonaws.com:5432/devdpu6hjoou93';
+const herokuDB = 'postgres://wnbqunwhpwfcvg:c6f3a0f189451c624e2da121b82eed34d26205083ebfe75c6e156819a770180f@ec2-54-75-248-193.eu-west-1.compute.amazonaws.com:5432/d1rd3sq2l3qj79';
 const localDB = 'postgres://localhost:5432/lunar-adventure';
 
 const db = new Sequelize(process.env.DATABASE_URL || herokuDB, {
